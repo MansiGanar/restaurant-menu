df13bc317b65ba05ddad20b9777deadf3f1a7662
Mansi Ganar <mansi99ganar@gmail.com>
2023-05-01 17:07:03 +0200

    Untracked file

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsPlayFill, BsPauseFill } from "react-icons/bs";
 import "./Intro.css";
 import { meal } from "../../constants";
 
 const Intro = () => {
+  const vidRef = React.useRef();
+  const [playVideo, setPlayVideo] = useState(false);
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo);
+
+    if (playVideo) {
+      vidRef.current.pause();
+    } else {
+      vidRef.current.play();
+    }
+  };
+
   return (
     <div>
-      <h1>Intro</h1>
+      <video
+        className="app__video"
+        src={meal}
+        ref={vidRef}
+        type="video/mp4"
+        Loop
+        controls={false}
+        muted
+      />
+      <div className="app_video-overlay flex__center">
+        <div
+          className="app__video-overlay_circle flex__center"
+          onClick={handleVideo}
+        >
+          {playVideo ? (
+            <BsPauseFill color="#ffff" fontSize={30} />
+          ) : (
+            <BsPlayFill color="#ffff" fontSize={30} />
+          )}
+        </div>
+      </div>
     </div>
   );
 };
